3191d396385c11a8851076c369bbc539b696a74c
Edoard00 <edo.arca@gmail.com>
2020-11-12 17:22:48 +0100

    Embedded MapBox to Vue.js

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,4 +1,36 @@
 <template>
-    <h2>My MapBox</h2>
-    <div id="mapView" ></div>
+  <link
+    href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+    rel="stylesheet"
+  />
+  <h2>My MapBox</h2>
+  <div id="mapView"></div>
 </template>
+
+<script>
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+
+export default {
+  name: "MapView",
+  data: function () {},
+  components: {},
+  mounted: async function () {
+    // Displaying the map
+    mapboxgl.accessToken = "pk.eyJ1IjoiZWQwYXJkMCIsImEiOiJja2gzZWZ3emMwOW9rMnlxeXE5dzhxOGpwIn0.mkzk2pwIF_2rKIDBu3LRVA";
+    new mapboxgl.Map({
+      container: "mapView",
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import url("https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css");
+#mapView {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
