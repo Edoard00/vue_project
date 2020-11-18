61c15707b66347dc18a7c69e5734cafe4e058570
Edoard00 <edo.arca@gmail.com>
2020-11-18 10:18:30 +0100

    gpx-route was deleted

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -9,19 +9,58 @@
 
 <script>
 import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
+//import contentfulClient from "@/modules/contentful.js"
+//import getCoordinatesFromGpxFile from "@/modules/gpx-utilities.js";
 
 export default {
   name: "MapView",
   mounted: async function () {
     // Displaying the map
-    mapboxgl.accessToken = "pk.eyJ1Ijoic3RlZmFua2Fpc2VyIiwiYSI6ImNraGYwdjg5ZTAyeDgycm1jeHBiMGdwbDIifQ.YYmhF-wM7ewea-ICXEPNIg"; // AccessToken Stefan Kaiser
-    new mapboxgl.Map({
+    mapboxgl.accessToken = "pk.eyJ1Ijoic3RlZmFua2Fpc2VyIiwiYSI6ImNraDNmZ3gyMzBvN3gydGxzazFyYW5wZW0ifQ.cMuMj2I3aRdFi0IDsULnbw"; // AccessToken Stefan Kaiser
+    /*const mapView = */new mapboxgl.Map({
       container: "mapView",
-      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      style: "mapbox://styles/stefankaiser/ckh3fl4n32m2y19qtlwckictn",
+      //style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
     });
-  },
+
+    // // Displaying a GPX track
+    // mapView.on("load", async function() {
+    //   let result = await contentfulClient.getEntries({
+    //     content_type: "coordinates"
+    //   });
+    //   //console.log(result);
+    //   let coordinates = await getCoordinatesFromGpxFile(
+    //     result.item[0].fields.gpx // a link to you gpx file in Contentful
+    //   );
+
+    //   mapView.addSource("route", {
+    //     type: "geojson",
+    //     data: {
+    //       type: "Feature",
+    //       geometry: {
+    //         type: "LineString",
+    //         coordinates: coordinates
+    //       }
+    //     }
+    //   });
+
+    //   mapView.addLayer({
+    //     id: "route",
+    //     type: "line",
+    //     source: "route",
+    //     layout: {
+    //       "line-join": "round",
+    //       "line-cap": "round"
+    //     },
+    //     paint: {
+    //       "line-color": "#888",
+    //       "line-width": 8
+    //     }
+    //   });
+    // });
+  }
 };
 </script>
 
@@ -29,6 +68,6 @@ export default {
 @import url("https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css");
 #mapView {
   height: 100vh;
-  width: 100vw;
+  width: 100%;
 }
 </style>
