1f7d9637d249b0134aa819a494720ee35ece80b7
stefan666kaiser <stefan.kaiser@stud.hslu.ch>
2020-11-19 16:24:23 +0100

    Commit before pull

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -5,21 +5,28 @@
     rel="stylesheet"
   />
 
+  <!-- <div class="background">
+    <div v-html="sectionsTextHtml"></div>
+    <p> {{ items.fields.text }} </p>
+  </div> -->
+
   <div class="black">
-    <h2>the white</h2>
+    <h2>The white</h2>
     <h1>Globetrotter</h1>
   </div>
 
-  <div class="black"></div>
+  <div class="black">
+    <p>Video T-shirt</p>
+  </div>
 
   <div class="white">
-    <h2>Jeder Schweizer kauft Jährlich</h2>
+    <h2>Jeder Schweizer, jede Schweizerin kauft jährlich</h2>
     <h1>28Kilogramm<br />Textilien.<br /></h1>
   </div>
 
   <div class="black">
-    <h1>500.000.000<br />T-Shirts<br /></h1>
-    <h2>werden in der Schweiz Jährlich verkauft.</h2>
+    <h1>50.000.000<br />T-Shirts<br /></h1>
+    <h2>werden in der Schweiz jährlich verkauft.</h2>
     <p>Bild</p>
   </div>
 
@@ -29,27 +36,30 @@
   </div>
 
   <div class="black">
-    <h2>In Deutschland landen jährlich in Abfall</h2>
-    <h1>1.500.000<br />Tonnen Textil.<br /></h1>
+    <h2>In der Schweiz landen jährlich</h2>
+    <h1>1.000.000<br />Tonnen Textil.<br /></h1>
+    <h2>im Abfall</h2>
   </div>
 
   <div class="white">
-    <h1>Nur 750.000<br />Tonnen Textil<br /></h1>
+    <h1>Nur 500.000<br />Tonnen Textil<br /></h1>
     <h2>werden recycelt.</h2>
   </div>
 
   <div class="black">
     <h2>Ein T-Shirt reist</h2>
     <h1>27.500<br />Kilometer<br /></h1>
-    <h2>bis es in die Schweiz ankommt.</h2>
+    <h2>bis es in der Schweiz ankommt.</h2>
   </div>
 
   <div class="white">
     <h2>Der Transport um die halbe Erde kostet</h2>
-    <h1>Pro T-shirt<br />Nur 0.10CHFr.<br /></h1>
+    <h1>pro T-shirt nur 10 Rappen</h1>
   </div>
 
-  <h2>Lass uns die Ganze Reise des T-Shirts anschauen</h2>
+  <div class="black"></div>
+
+  <h2>Lass uns die ganze Reise des T-Shirts anschauen</h2>
   <div id="mapView"></div>
   <div id="features">
     <section id="baker" class="active">
@@ -141,33 +151,37 @@ section:last-child {
   border-bottom: none;
   margin-bottom: 200px;
 }
-section.black {
-  background-color: #000000;
-}
 .black {
   background-color: black;
-  h1,
-  h2 {
+  h1, h2 {
     color: white;
   }
 }
 .white {
   background-color: #ffffff;
-  h1,
-  h2 {
+  h1, h2 {
     color: black;
   }
 }
 div {
-  height: 100vw;
+  height: 90vw;
   margin-top: -16px;
 }
-h1, h2 {
+h1,
+h2 {
   font-weight: bold;
+
 }
 h1 {
-  font-size: 8vw;
+  font-size: 16vw;
 }
+h2 {
+  font-size: 4vw;
+  
+
+}
+
+
 </style>
 // ================================
 
@@ -184,6 +198,7 @@ export default {
   // },
   data() {
     return {
+      sectionsTextHtml: [],
       sections: [],
     };
   },
